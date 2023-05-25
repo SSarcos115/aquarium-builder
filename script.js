c7a3841c3d88ee7866a4d5b122212bf1d4425968
Sebastian <bastians12@gmail.com>
2023-05-25 00:39:57 -0400

    Fix search toggle & login/register page validation

--- script.js
+++ script.js
@@ -1,14 +1,30 @@
 
-
-function searchBar() {
-    let searchIcon = document.getElementById("searchBar_wrapper");
-    if (searchIcon.style.display === "none") {
-      searchIcon.style.display = "block";
-    } else {
-      searchIcon.style.display = "none";
-    }
+// Search bar toggle
+function searchBarOn() {
+  let searchIcon = document.getElementById("searchBar_wrapper")
+  if (window.getComputedStyle(searchIcon).display === "none") {
+    searchIcon.style.display = "block";
+  } else {
+    searchIcon.style.display = "none";
   }
-
-window.onclick() = function() {
-  
 }
+
+
+// Register Page password match validation
+let password = document.getElementById("password")
+let confirm_password = document.getElementById("confirm_password");
+
+function validatePassword(event){
+  if(password.value != confirm_password.value) {
+    confirm_password.setCustomValidity("Passwords Don't Match");
+    confirm_password.reportValidity(); 
+    event.preventDefault();
+  } else {
+    confirm_password.setCustomValidity('');
+  }
+}
+
+
+password.onchange = validatePassword;
+confirm_password.onkeyup = validatePassword;
+
